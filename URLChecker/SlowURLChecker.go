3886c915e7f5e4cb96cd1a3ded812c99066db035
ur2e <dbfl9711@gamil.com>
2023-06-07 18:58:36 +0900

    [CH3] URLChecker: goroutine 적용 전

---
+++ URLChecker/SlowURLChecker.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var errRequestFailed = errors.New("Request failed")
+
+func main() {
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+
+	for _, url := range urls {
+		hitURL(url)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	time.Sleep(time.Second)
+	return nil
+}
